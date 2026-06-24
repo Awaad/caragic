8a0f88e9b51e31acc33ca5bcbbfc244e28ea7cb8
abdelrahman <abdelrahman.awad1@gmail.com>
2026-06-25 01:45:16 +0300

    chore(deps): add vite-plugin-glsl for shader imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+          glsl({
+            include: ['**/*.glsl', '**/*.vert', '**/*.frag'],
+            defaultExtension: 'glsl',
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
